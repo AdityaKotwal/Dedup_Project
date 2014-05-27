e5f34c9f8188643356798c74b2eb9a2e934dea0a
Aditya Kotwal <akotwal@akotwal-mbpro.prom.eng.vmware.com>
2014-05-27 11:29:01 -0700

    Added wrapper for error checking

--- Dedup_Project/main.c
+++ Dedup_Project/main.c
@@ -10,7 +10,8 @@
 #include <stdlib.h>
 #include <errno.h>
 #define FNAME "/Users/akotwal/Desktop/Dedup_Project/duplicateFile"
-#define verbose 1
+#define verbose 0
+#define fileNameRequired 0
 
 // MD5 Libraries:
 #if defined(__APPLE__)
@@ -25,8 +26,22 @@
 //#define MAX_BUF_LEN 4*1024
 #define MAX_BUF_LEN 4
 #define RDLEN MAX_BUF_LEN
-#define CRYPT_LEN 20
+#define CRYPT_LEN 16
 
+// Error checking wrapper for fwrite
+void FWRITE(char *buf,char *msg, FILE* stream){
+    if(fwrite(buf,1,strlen(buf),stream)!=strlen(buf)){
+        printf("Error in writing %s to file\n",msg);
+    }
+}
+//Error checking wrapper for fopen
+FILE* FOPEN(char *fileName, char* mode){
+    FILE* fp = NULL;
+    if((fp=fopen(fileName,mode))==NULL){
+        printf("Failed to open file %s in %s mode\n",fileName,mode);
+    }
+    return fp;
+}
 
 int main(int argc, const char * argv[])
 {
@@ -34,31 +49,28 @@ int main(int argc, const char * argv[])
     char fileName[MAX_FILE_NAME];
     strcpy(fileName,FNAME);
     FILE *ip,*op;
-    if((ip = fopen(fileName,"rb"))==NULL){
-        printf("File could not be opened, error, %d\n",errno);
-    }
-    if((op = fopen("/Users/akotwal/Desktop/Dedup_Project/outfile","wb"))==NULL){
-        printf("File could not be opened, error, %d\n",errno);
-    }
-    
-    char header[MAX_FILE_NAME];
-    if(verbose){
-        strcpy(header,"String, File Name, md5, offset, size \n");
-        if(fwrite(header,1,strlen(header),op)!=strlen(header)){
-            printf("Error in writing filename\n");
-        }
-    }
-    char *buf = NULL;
-    if((buf = malloc(4*1024)) == NULL){
-        printf("Error in allocating memory");
-    }
-    char *buf_orig=buf;
     char out[16*2+1];
     unsigned long len;
     unsigned char final[16];
     unsigned long offset=0;
     unsigned long offsetCorrection=0;
-    char *buf_read = buf;
+    char *buf = NULL, *buf_orig = NULL, *buf_read=NULL;
+    
+    // Opening input and output file
+    ip = FOPEN(fileName,"rb");
+    op = FOPEN("/Users/akotwal/Desktop/Dedup_Project/outfile","wb");
+    
+    char header[MAX_FILE_NAME];
+    if(verbose){
+        strcpy(header,"String, File Name, md5, offset, size \n");
+        FWRITE(header, "header", op);
+    }
+
+    if((buf = malloc(4*1024)) == NULL){
+        printf("Error in allocating memory");
+    }
+    buf_orig=buf;
+    buf_read = buf;
     while((len=fread(buf,1,RDLEN,ip))!=0){
         MD5_Init(&c);
         offsetCorrection=len;
@@ -74,45 +86,30 @@ int main(int argc, const char * argv[])
             }
         }
         MD5_Final(final, &c);
-        for (int n = 0; n < CRYPT_LEN; ++n) {
-            snprintf(&(out[n*2]), CRYPT_LEN*2, "%02x",final[n]);
+        for (int n = 0; n < 16; ++n) {
+            snprintf(&(out[n*2]), 16*2, "%02x",final[n]);
         }
         if(verbose){
-            if(fwrite(buf_read,1,strlen(buf_read),op)!=strlen(buf_read)){
-                printf("Error in writing buf\n");
-            }
-            if(fwrite(",",1,strlen(","),op)!=strlen(",")){
-                printf("Error in writing ,\n");
-            }
+            FWRITE(buf_read, "buf",op);
+            FWRITE(",",",",op);
         }
-        if(fwrite(fileName,1,strlen(fileName),op)!=strlen(fileName)){
-            printf("Error in writing filename\n");
+        if(fileNameRequired){
+            FWRITE(fileName, "fileName",op);
+            FWRITE(",",",",op);
         }
-        if(fwrite(",",1,strlen(","),op)!=strlen(",")){
-            printf("Error in writing filename\n");
-        }
-        if(fwrite(out,1,strlen(out),op)!=strlen(out)){
-            printf("Error in writing md5 value\n");
-        }
-        if(fwrite(",",1,strlen(","),op)!=strlen(",")){
-            printf("Error in writing f ','\n");
-        }
+        FWRITE(out,"md5value",op);
+        FWRITE(",",",",op);
+
         char offset_str[MAX_BUF_LEN];
         sprintf(offset_str, "%lu",offset);
-        if(fwrite(offset_str,1,strlen(offset_str),op)!=strlen(offset_str)){
-            printf("Unable to write offest\n");
-        }
-        if(fwrite(",",1,strlen(","),op)!=strlen(",")){
-            printf("Error in writing ','\n");
-        }
+        FWRITE(offset_str,"offset",op);
+        FWRITE(",",",",op);
+        
         char offsetCorr_str[MAX_BUF_LEN];
         sprintf(offsetCorr_str, "%lu",offsetCorrection);
-        if(fwrite(offsetCorr_str,1,strlen(offsetCorr_str),op)!=strlen(offsetCorr_str)){
-            printf("Unable to write size\n");
-        }
-        if(fwrite("\n",1,strlen("\n"),op)!=strlen("\n")){
-            printf("Error in writing newline\n");
-        }
+        FWRITE(offsetCorr_str,"size",op);
+        FWRITE(",",",",op);
+        
         offset += offsetCorrection;
         buf=buf_orig;
     }
